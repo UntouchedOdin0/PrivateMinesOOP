86cc4c8181c834cb5b4a3e271ec693db277704bd
UntouchedOdin0 <kylehicksuk@gmail.com>
2021-09-16 22:28:38 +0100

    Implemented some getters and setters.

--- src/main/java/me/untouchedodin0/privatemines/PrivateMines.java
+++ src/main/java/me/untouchedodin0/privatemines/PrivateMines.java
@@ -4,23 +4,37 @@ import me.untouchedodin0.privatemines.mines.Mine;
 import org.bukkit.Material;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.EnumMap;
+
 public class PrivateMines extends JavaPlugin {
+
+    EnumMap<Material, Double> mineBlocks = new EnumMap<>(Material.class);
 
     @Override
     public void onEnable() {
+        mineBlocks.put(Material.STONE, 0.5);
+        mineBlocks.put(Material.EMERALD_ORE, 0.5);
+
         Mine mine = new Mine();
-        mine.name = "Mine";
-        mine.mineBlocks.put(Material.STONE, 0.1);
-        mine.mineBlocks.put(Material.EMERALD_BLOCK, 0.9);
-        mine.resetTime = 1;
-        mine.type = "Type";
-        mine.mineLocation = null;
+        mine.setName("Mine");
+        mine.setMineBlocks(mineBlocks);
+        mine.setTier(1);
+        mine.setResetTime(5);
+
+        Mine mine2 = new Mine();
+        mine2.setName("Mine2");
+        mine2.setMineBlocks(mineBlocks);
+        mine2.setTier(2);
+        mine2.setResetTime(5);
 
         System.out.println("Mine Name: " + mine.getName());
+        System.out.println("Mine Tier: " + mine.getMineTier());
         System.out.println("Mine Blocks: " + mine.getMineBlocks());
         System.out.println("Mine Reset Time: " + mine.getResetTime());
-        System.out.println("Mine Type: " + mine.getType());
-        System.out.println("Cuboid Region: " + mine.getCuboidRegion());
-        System.out.println("Mine Location: " + mine.getMineLocation());
+
+        System.out.println("Mine2 Name: " + mine2.getName());
+        System.out.println("Mine2 Tier: " + mine2.getMineTier());
+        System.out.println("Mine2 Blocks: " + mine2.getMineBlocks());
+        System.out.println("Mine2 Reset Time: " + mine2.getResetTime());
     }
 }
--- src/main/java/me/untouchedodin0/privatemines/mines/Mine.java
+++ src/main/java/me/untouchedodin0/privatemines/mines/Mine.java
@@ -8,38 +8,51 @@ import java.util.EnumMap;
 
 public class Mine {
 
-    public String name = "";
-    public String type = "";
+    private String name;
 
-    public EnumMap<Material, Double> mineBlocks = new EnumMap<>(Material.class);
+    private EnumMap<Material, Double> mineBlocks = new EnumMap<>(Material.class);
 
-    public int resetTime = 1;
+    private int mineTier = 1;
+    private int resetTime = 1;
 
-    public Location mineLocation;
+    private Location mineLocation;
 
     private CuboidRegion cuboidRegion;
+
+    public void setName(String mineName) {
+        this.name = mineName;
+    }
+
+    public void setMineBlocks(EnumMap<Material, Double> mineBlocks) {
+        this.mineBlocks = mineBlocks;
+    }
+
+    public void setTier(int tier) { this.mineTier = tier; }
+
+    public void setResetTime(int time) {
+        this.resetTime = time;
+    }
+
+    public void setCuboidRegion(CuboidRegion cuboidRegion) {
+        this.cuboidRegion = cuboidRegion;
+    }
+
+    public void setMineLocation(Location mineLocation) {
+        this.mineLocation = mineLocation;
+    }
+
 
     public String getName() {
         return name;
     }
 
-    public EnumMap<Material, Double> getMineBlocks() {
-        return mineBlocks;
-    }
+    public EnumMap<Material, Double> getMineBlocks() { return mineBlocks; }
 
-    public String getType() {
-        return type;
-    }
+    public int getMineTier() { return mineTier; }
 
-    public int getResetTime() {
-        return resetTime;
-    }
+    public int getResetTime() { return resetTime; }
 
-    public CuboidRegion getCuboidRegion() {
-        return cuboidRegion;
-    }
+    public Location getMineLocation() { return mineLocation; }
 
-    public Location getMineLocation() {
-        return mineLocation;
-    }
+    public CuboidRegion getCuboidRegion() { return cuboidRegion; }
 }
