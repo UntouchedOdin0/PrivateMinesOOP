9951cf47fa39a06e5a8b2c5ea712bfe2245511d1
UntouchedOdin0 <kylehicksuk@gmail.com>
2021-09-19 22:02:03 +0100

    .

--- src/main/java/me/untouchedodin0/privatemines/world/utils/Direction.java
+++ src/main/java/me/untouchedodin0/privatemines/world/utils/Direction.java
@@ -5,9 +5,9 @@ import org.bukkit.Location;
 public enum Direction {
 
     NORTH(0, -1), NORTH_EAST(1, -1),
-    EAST(1, 0), SOUTH_EAST(1, 1),
-    SOUTH(0, 1), SOUTH_WEST(-1, 1),
-    WEST(-1, 0), NORTH_WEST(-1, -1);
+    EAST(1, 0),   SOUTH_EAST(1, 1),
+    SOUTH(0, 1),  SOUTH_WEST(-1, 1),
+    WEST(-1, 0),  NORTH_WEST(-1, -1);
 
     private final int xMulti;
     private final int zMulti;
