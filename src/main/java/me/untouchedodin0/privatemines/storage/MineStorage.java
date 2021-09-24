7bfef19975a95daa23604891eebb6edb5e6cb14c
UntouchedOdin0 <kylehicksuk@gmail.com>
2021-09-24 17:53:29 +0100

    Adding comments to MineStorage class

--- src/main/java/me/untouchedodin0/privatemines/storage/MineStorage.java
+++ src/main/java/me/untouchedodin0/privatemines/storage/MineStorage.java
@@ -8,33 +8,37 @@ import java.util.UUID;
 
 public class MineStorage {
 
+    /**
+     *  The map of all the mines and their owner's UUID.
+     */
+
     private final Map<UUID, Mine> mines = new HashMap<>();
+
+    /**
+     *
+     * @param uuid - The UUID Of the mine owner to be added to the storage
+     * @param mine - The Mine to be added into the storage
+     */
 
     public void addMine(UUID uuid, Mine mine) {
         mines.putIfAbsent(uuid, mine);
     }
 
+    /**
+     *
+     * @param uuid - The UUID of the player who's mine we'll be deleting from storage
+     */
+
     public void removeMine(UUID uuid) {
         mines.remove(uuid);
     }
 
+    /**
+     *
+     * @return A map of all the mines with the owners UUID's.
+     */
+
     public Map<UUID, Mine> getMines() {
         return mines;
     }
-
-    /*
-    public void addMineData(String string, MineData mineData) {
-        mineDataMap.putIfAbsent(string, mineData);
-    }
-
-    public void removeMineData(MineData mineData) {
-        if (!mineDataMap.containsValue(mineData)) return;
-        Bukkit.getLogger().info("Removing mineData " + mineData.getName() + " from mine storage");
-        mineDataMap.remove(mineData.getName());
-    }
-
-    public Map<String, MineData> getMineDataMap() {
-        return mineDataMap;
-    }
-    */
 }
