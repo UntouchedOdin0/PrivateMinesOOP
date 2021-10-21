0b6bb41cc5dd82e2ba711f4e78794982463a5e6e
UntouchedOdin0 <kylehicksuk@gmail.com>
2021-10-21 17:35:35 +0100

    we do a little tidy up

--- src/main/java/me/untouchedodin0/privatemines/factory/MineFactory.java
+++ src/main/java/me/untouchedodin0/privatemines/factory/MineFactory.java
@@ -29,7 +29,6 @@ import me.untouchedodin0.privatemines.mines.Mine;
 import me.untouchedodin0.privatemines.mines.MineType;
 import me.untouchedodin0.privatemines.storage.MineStorage;
 import me.untouchedodin0.privatemines.util.Utils;
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
@@ -63,7 +62,6 @@ public class MineFactory {
             privateMines.getLogger().warning("Failed to create mine due to defaultMineData being null");
         }
 
-        Utils utils = new Utils(privateMines);
         Block block = location.getBlock();
         String userUUID = player.getUniqueId().toString();
         Mine mine = new Mine(privateMines);
@@ -72,17 +70,6 @@ public class MineFactory {
         mine.setMineType(defaultMineType);
         mine.setWeightedRandom(defaultMineType.getWeightedRandom());
         mine.build();
-
-//        Location corner1 = utils.getRelative(mine.getStructure(), defaultMineType.getCorner1());
-//        Location corner2 = utils.getRelative(mine.getStructure(), defaultMineType.getCorner2());
-//
-//        Location spawnLocation = utils.getRelative(mine.getStructure(), defaultMineType.getSpawnLocation());
-//        Location npcLocation = utils.getRelative(mine.getStructure(), defaultMineType.getNpcLocation());
-//
-//        mine.setCorner1(corner1);
-//        mine.setCorner2(corner2);
-//        mine.setSpawnLocation(spawnLocation);
-//        mine.setNpcLocation(npcLocation);
 
         mineStorage.addMine(player.getUniqueId(), mine);
 
@@ -97,50 +84,7 @@ public class MineFactory {
         dataBlock.set("structure", mine.getStructure());
 
         blockDataManager.save();
-        DataBlock mineDataBlock = blockDataManager.getExisting(block);
-        if (mineDataBlock != null) {
-            String owner = mineDataBlock.getString("owner");
-            String type = mineDataBlock.getString("type");
-            String locationString = mineDataBlock.getString("location");
-            String spawnLocationString = mineDataBlock.getString("spawnLocation");
-            String npcLocationString = mineDataBlock.getString("npcLocation");
-            String corner1String = mineDataBlock.getString("corner1");
-            String corner2String = mineDataBlock.getString("corner2");
-//            String structureString = mineDataBlock.getString("structure");
-
-            Bukkit.getLogger().info("owner: " + owner);
-            Bukkit.getLogger().info("type: " + type);
-            Bukkit.getLogger().info("location: " + locationString);
-            Bukkit.getLogger().info("spawnLocation: " + spawnLocationString);
-            Bukkit.getLogger().info("npcLocation: " + npcLocationString);
-
-            Bukkit.getLogger().info("corner1: " + corner1String);
-            Bukkit.getLogger().info("corner2: " + corner2String);
-        }
-
-//        String mines = "mines.";
-//        String mineType = mine.getMineData().getName();
-//        minesConfig.getConfig().set(mines + userUUID + ".type", mineType);
-//        minesConfig.getConfig().set(mines + userUUID + ".location", location);
-//        minesConfig.getConfig().set(mines + userUUID + ".spawnLocation", mine.getSpawnLocation());
-//        minesConfig.getConfig().set(mines + userUUID + ".npcLocation", mine.getNpcLocation());
-//        minesConfig.getConfig().set(mines + userUUID + ".corner1", mine.getCuboidRegion().getStart());
-//        minesConfig.getConfig().set(mines + userUUID + ".corner2", mine.getCuboidRegion().getEnd());
-//        minesConfig.getConfig().set(mines + userUUID + ".mineOwner", mine.getMineOwner().toString());
-//        minesConfig.getConfig().set(mines + userUUID + ".weightedRandom", mine.getWeightedRandom().toString());
-//        minesConfig.getConfig().set(mines + userUUID + ".isAutoResetting", mine.isAutoResetting());
-//        minesConfig.save();
-
-        //todo fix this reset thing
         mine.reset();
-
-
-
-//        if (debugMode) {
-//            privateMines.getLogger().info("createMine block: " + block);
-//            privateMines.getLogger().info("createMine dataBlock: " + dataBlock);
-//            privateMines.getLogger().info("createMine dataBlock getData: " + dataBlock.getData());
-//        }
         return mine;
     }
 
