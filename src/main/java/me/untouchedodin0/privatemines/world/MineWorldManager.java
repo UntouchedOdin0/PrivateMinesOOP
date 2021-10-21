7de192d58fc5d17531757386d7a60d64f96dd6c6
UntouchedOdin0 <kylehicksuk@gmail.com>
2021-10-21 17:30:15 +0100

    no need in this

--- src/main/java/me/untouchedodin0/privatemines/world/MineWorldManager.java
+++ src/main/java/me/untouchedodin0/privatemines/world/MineWorldManager.java
@@ -29,24 +29,19 @@ import static me.untouchedodin0.privatemines.world.utils.Direction.NORTH;
 
 public class MineWorldManager {
 
-    private final World minesWorld;
     private final Location defaultLocation;
     private final int borderDistance;
     private int distance = 0;
     private me.untouchedodin0.privatemines.world.utils.Direction direction;
 
     public MineWorldManager() {
-        this.minesWorld = Bukkit.createWorld(
+        World minesWorld = Bukkit.createWorld(
                 new WorldCreator("privatemines")
-                .type(WorldType.FLAT)
-                .generator(new EmptyWorldGenerator()));
+                        .type(WorldType.FLAT)
+                        .generator(new EmptyWorldGenerator()));
         this.borderDistance = 150;
         this.direction = NORTH;
         defaultLocation = new Location(minesWorld, 0, 1, 0); // may need to raise the Y sometime?
-    }
-
-    public World getMinesWorld() {
-        return minesWorld;
     }
 
     public synchronized Location getNextFreeLocation() {
