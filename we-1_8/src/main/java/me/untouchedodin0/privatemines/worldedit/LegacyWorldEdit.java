4cab36b7302671e136b5dc80e67deae2ab6a9fef
UntouchedOdin0 <kylehicksuk@gmail.com>
2021-10-26 22:39:25 +0100

    ugh.....

--- we-1_8/src/main/java/me/untouchedodin0/privatemines/worldedit/LegacyWorldEdit.java
+++ we-1_8/src/main/java/me/untouchedodin0/privatemines/worldedit/LegacyWorldEdit.java
@@ -1,4 +1,13 @@
 package me.untouchedodin0.privatemines.worldedit;
 
+import com.sk89q.worldedit.LocalSession;
+import com.sk89q.worldedit.WorldEdit;
+import com.sk89q.worldedit.session.SessionManager;
+
 public class LegacyWorldEdit {
+
+    SessionManager sessionManager = WorldEdit.getInstance().getSessionManager();
+    LocalSession localSession;
+    Utils utils = new Utils();
+
 }
