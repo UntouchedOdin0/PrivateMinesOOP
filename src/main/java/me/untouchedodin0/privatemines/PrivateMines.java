44f04ac480fe89face844a268326f003e775a902
UntouchedOdin0 <kylehicksuk@gmail.com>
2021-10-27 16:49:02 +0100

    adding placeholderapi support

--- src/main/java/me/untouchedodin0/privatemines/PrivateMines.java
+++ src/main/java/me/untouchedodin0/privatemines/PrivateMines.java
@@ -34,6 +34,7 @@ import me.untouchedodin0.privatemines.mines.MineType;
 import me.untouchedodin0.privatemines.storage.MineStorage;
 import me.untouchedodin0.privatemines.util.Metrics;
 import me.untouchedodin0.privatemines.util.Utils;
+import me.untouchedodin0.privatemines.util.placeholderapi.PrivateMinesExpansion;
 import me.untouchedodin0.privatemines.world.MineWorldManager;
 import me.untouchedodin0.privatemines.world.utils.MineLoopUtil;
 import org.bukkit.Bukkit;
@@ -203,6 +204,13 @@ public class PrivateMines extends JavaPlugin {
         getLogger().info("Loading worldguard wrapper version: " + WorldGuardWrapper.getInstance().getApiVersion());
         getLogger().info("Loading worldedit version 1_12: " + worldEditVersion1_12);
         getLogger().info("Loading worldedit version 1_13: " + worldEditVersion1_13);
+
+        if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
+            getLogger().info("Connecting to placeholder api and registering the placeholders");
+            new PrivateMinesExpansion(mineStorage).register();
+        } else {
+            getLogger().info("PlaceholderAPI was not present, not able to establish a hook!");
+        }
     }
 
     /*
