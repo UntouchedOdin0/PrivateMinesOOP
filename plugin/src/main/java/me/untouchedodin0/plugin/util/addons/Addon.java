ce3d84f9d10439fa764fd6d6d23d3e511e998bcd
UntouchedOdin0 <kylehicksuk@gmail.com>
2022-01-28 17:47:28 +0000

    add enable disable and state shit for addons

--- plugin/src/main/java/me/untouchedodin0/plugin/util/addons/Addon.java
+++ plugin/src/main/java/me/untouchedodin0/plugin/util/addons/Addon.java
@@ -1,7 +1,54 @@
 package me.untouchedodin0.plugin.util.addons;
 
+import java.io.File;
+
 public abstract class Addon {
 
     private static final String ADDON_CONFIG_FILENAME = "config.yml";
+    private State state;
+    private File file;
 
+    protected Addon() {
+        state = State.DISABLED;
+    }
+
+    /**
+     * Executes the code when enabling the addon.
+     */
+
+    public abstract void onEnable();
+
+    /**
+     * Executes the code when disabling the addon
+     */
+
+    public abstract void onDisable();
+
+    /**
+     * Represents the current run-time state of a {@link Addon}.
+     *
+     * @author Poslovitch
+     */
+    public enum State {
+
+        /**
+         * The addon has been correctly loaded.
+         */
+        LOADED,
+
+        /**
+         * The addon has been correctly enabled and is now fully working.
+         */
+        ENABLED,
+
+        /**
+         * The addon is fully disabled.
+         */
+        DISABLED,
+
+        /**
+         * The addon loading or enabling process has been interrupted by an unhandled error.
+         */
+        ERROR
+    }
 }
--- plugin/src/main/java/me/untouchedodin0/plugin/util/addons/request/AddonRequestHandler.java
+++ plugin/src/main/java/me/untouchedodin0/plugin/util/addons/request/AddonRequestHandler.java
@@ -1,4 +1,22 @@
 package me.untouchedodin0.plugin.util.addons.request;
 
+import java.util.Locale;
+
 public class AddonRequestHandler {
+
+    private final String label;
+
+    protected AddonRequestHandler(String label) {
+        this.label = label.toLowerCase(Locale.ENGLISH);
+    }
+
+    /**
+     * Get request handler label
+     *
+     * @return label
+     */
+
+    public String getLabel() {
+        return label;
+    }
 }
