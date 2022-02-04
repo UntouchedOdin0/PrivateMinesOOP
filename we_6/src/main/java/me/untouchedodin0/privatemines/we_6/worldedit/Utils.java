0c1d7873cecdd94a2993cb0219622ad4447f587f
UntouchedOdin0 <kylehicksuk@gmail.com>
2022-02-04 12:35:11 +0000

    removed some unused things and updated version

--- we_6/src/main/java/me/untouchedodin0/privatemines/we_6/worldedit/Utils.java
+++ we_6/src/main/java/me/untouchedodin0/privatemines/we_6/worldedit/Utils.java
@@ -24,13 +24,9 @@ SOFTWARE.
 
 package me.untouchedodin0.privatemines.we_6.worldedit;
 
-import com.cryptomorin.xseries.XMaterial;
-import com.sk89q.worldedit.blocks.BlockType;
-import org.bukkit.Material;
-
 public class Utils {
 
-    public BlockType bukkitToBlockType(Material material) {
-        return BlockType.fromID(XMaterial.matchXMaterial(material).getId());
-    }
+//    public BlockType bukkitToBlockType(Material material) {
+//        return BlockType.fromID(XMaterial.matchXMaterial(material).getId());
+//    }
 }
