de92f0e2efe6ce1edc304374385ced06a2251803
UntouchedOdin0 <kylehicksuk@gmail.com>
2022-02-19 14:54:01 +0000

    iteration optimization

--- plugin/src/main/java/me/untouchedodin0/plugin/factory/MineFactory.java
+++ plugin/src/main/java/me/untouchedodin0/plugin/factory/MineFactory.java
@@ -64,15 +64,17 @@ public class MineFactory {
         mineBlocks.corners = new Location[2];
         mineRegion.forEachBlock(mineBlock -> {
             Material bukkitMaterial = mineBlock.getType();
-            if (bukkitMaterial == spawnMaterial) {
-                mineBlocks.spawnLocation = mineBlock.getLocation();
-            } else if (bukkitMaterial == (cornerMaterial)) {
-                if (mineBlocks.corners[0] == null) {
-                    mineBlocks.corners[0] = mineBlock.getLocation();
-                } else if (mineBlocks.corners[1] == null) {
-                    mineBlocks.corners[1] = mineBlock.getLocation();
-                } else {
-                    throw new IllegalArgumentException("Too many corners in mine!");
+            if (bukkitMaterial.isSolid()) {
+                if (bukkitMaterial == spawnMaterial) {
+                    mineBlocks.spawnLocation = mineBlock.getLocation();
+                } else if (bukkitMaterial == (cornerMaterial)) {
+                    if (mineBlocks.corners[0] == null) {
+                        mineBlocks.corners[0] = mineBlock.getLocation();
+                    } else if (mineBlocks.corners[1] == null) {
+                        mineBlocks.corners[1] = mineBlock.getLocation();
+                    } else {
+                        throw new IllegalArgumentException("Too many corners in mine!");
+                    }
                 }
             }
         });
@@ -90,9 +92,6 @@ public class MineFactory {
         Material spawnMaterial = Config.spawnPoint;
         Material mineCornerMaterial = Config.mineCorner;
         boolean useFAWE = Config.useFAWE;
-
-        privateMines.getLogger().info(spawnMaterial.name());
-        privateMines.getLogger().info(mineCornerMaterial.name());
 
         File schematicFile = new File("plugins/PrivateMines/schematics/" + mineType.getFile());
         Path path = schematicFile.toPath();
