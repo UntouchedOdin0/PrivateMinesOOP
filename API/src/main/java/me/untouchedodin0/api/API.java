bbe0619ae750db4bec5e813974afcf58a17acf8e
UntouchedOdin0 <kylehicksuk@gmail.com>
2022-02-20 18:01:57 +0000

    Use the API to get the mine factory and storage

--- API/src/main/java/me/untouchedodin0/api/API.java
+++ API/src/main/java/me/untouchedodin0/api/API.java
@@ -3,6 +3,7 @@ package me.untouchedodin0.api;
 import lombok.Getter;
 import me.untouchedodin0.plugin.PrivateMines;
 import me.untouchedodin0.plugin.PrivateMinesAPI;
+import me.untouchedodin0.plugin.factory.MineFactory;
 import me.untouchedodin0.plugin.mines.Mine;
 import me.untouchedodin0.plugin.storage.MineStorage;
 
@@ -16,7 +17,8 @@ public class API {
     PrivateMines privateMines = PrivateMines.getPrivateMines();
     PrivateMinesAPI privateMinesAPI = PrivateMines.getAPI();
 
-    MineStorage mineStorage = privateMines.getMineStorage();
+    MineFactory mineFactory = privateMinesAPI.getMineFactory();
+    MineStorage mineStorage = privateMinesAPI.getMineStorage();
 
     public Mine getMine(UUID uuid) {
         return mineStorage.getMine(uuid);
