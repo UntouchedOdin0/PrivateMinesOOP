56d57008a39480a9bf3c91ac1f2e21fffe1e8fdf
UntouchedOdin0 <kylehicksuk@gmail.com>
2022-02-25 15:37:11 +0000

    add note

--- plugin/src/main/java/me/untouchedodin0/plugin/listener/MineCreationTest.java
+++ plugin/src/main/java/me/untouchedodin0/plugin/listener/MineCreationTest.java
@@ -1,7 +1,6 @@
 package me.untouchedodin0.plugin.listener;
 
 import me.untouchedodin0.plugin.events.PrivateMineCreationEvent;
-import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
@@ -9,6 +8,6 @@ public class MineCreationTest implements Listener {
 
     @EventHandler
     public void onCreate(PrivateMineCreationEvent event) {
-        Bukkit.broadcastMessage("Mine: " + event.getMine() + " has been created!");
+        // Write the code here of which you'd like to execute when the mine is created.
     }
 }
