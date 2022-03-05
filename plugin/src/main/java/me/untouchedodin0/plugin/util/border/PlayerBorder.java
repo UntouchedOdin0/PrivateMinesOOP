108bc09afc8f2b9bff9af7b97697441c1edf9e5a
UntouchedOdin0 <kylehicksuk@gmail.com>
2022-03-05 21:59:34 +0000

    border changes

--- plugin/src/main/java/me/untouchedodin0/plugin/util/border/PlayerBorder.java
+++ plugin/src/main/java/me/untouchedodin0/plugin/util/border/PlayerBorder.java
@@ -2,6 +2,7 @@ package me.untouchedodin0.plugin.util.border;
 
 import com.github.retrooper.packetevents.PacketEvents;
 import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerHeldItemChange;
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerWorldBorder;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
@@ -9,6 +10,9 @@ public class PlayerBorder {
 
     public void sendBorder(Player player, Location location) {
         WrapperPlayServerHeldItemChange packet = new WrapperPlayServerHeldItemChange(1);
-        PacketEvents.getAPI().getPlayerManager().sendPacket(player, packet);
+        WrapperPlayServerWorldBorder wrapperPlayServerWorldBorder = new WrapperPlayServerWorldBorder(5);
+        WrapperPlayServerWorldBorder test = new WrapperPlayServerWorldBorder(location.getX(), location.getY());
+
+        PacketEvents.getAPI().getPlayerManager().sendPacket(player, test);
     }
 }
